35a21d92d9cae649e7dc635250f3fcc169b9fa91
nu20 <bibhutipatra123@gmail.com>
2024-10-05 18:42:25 +0530

    first commit

---
+++ tur.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("red")
+turtle.Screen().setup(300,400)
+polygon = turtle.Turtle()
+for i in range (4):
+    polygon.forward (90)
+    polygon.right (90)
+turtle.done ()
